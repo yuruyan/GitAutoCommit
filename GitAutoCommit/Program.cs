154ff312911c2ccf1f349c08c5d85ca0274638d6
Luxiu123 <klt5466@163.com>
2022-07-09 16:22:44 +0800

    mod: 完善 program

--- GitAutoCommit/Program.cs
+++ GitAutoCommit/Program.cs
@@ -2,6 +2,8 @@
 //Console.WriteLine("Hello, World!");
 
 using LibGit2Sharp;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.CommandLine;
 
 namespace GitAutoCommit;
 
@@ -19,15 +21,58 @@ class App {
     /// </summary>
     private string TargetModifyFile;
 
+    private record CommandArg {
+        public uint CommitTimes { get; set; } = 1;
+        public string RepoPath { get; set; } = string.Empty;
+    }
+
+    /// <summary>
+    /// 构造函数
+    /// </summary>
+    /// <param name="repoPath">如果是 null 或者空串，则为 DefaultRepositoryPath</param>
+    /// <see cref="DefaultRepositoryPath"/>
     public App(string repoPath = DefaultRepositoryPath) {
-        RepositoryPath = repoPath;
+        if (!string.IsNullOrWhiteSpace(repoPath)) {
+            RepositoryPath = repoPath;
+        }
         TargetModifyFile = Path.Combine(RepositoryPath, @"GitAutoCommit\Demo.txt");
         CheckRepository();
     }
 
+    /// <summary>
+    /// 解析命令行参数
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    private static CommandArg ParseCommandArgs(string[] args) {
+        var configRoot = new ConfigurationBuilder().AddCommandLine(args).Build();
+        var repoPath = configRoot.GetSection("repo").Value;
+        var times = configRoot.GetSection("times").Value;
+        var result = new CommandArg() {
+            RepoPath = repoPath ?? string.Empty,
+        };
+        if (!string.IsNullOrWhiteSpace(times)) {
+            if (uint.TryParse(times, out var tempTimes)) {
+                result.CommitTimes = tempTimes;
+            }
+        }
+        return result;
+    }
+
+    /// <summary>
+    /// --repo 指定仓库路径
+    /// --times 指定提交次数
+    /// </summary>
+    /// <param name="args"></param>
     static void Main(string[] args) {
+        CommandArg commandArg = ParseCommandArgs(args);
+        var app = new App(commandArg.RepoPath);
         try {
-            new App().Run();
+            for (int i = 0; i < commandArg.CommitTimes; i++) {
+                app.Run();
+                Console.WriteLine($"commit {i + 1} times");
+            }
+            Console.WriteLine("success");
         } catch (Exception e) {
             Console.WriteLine(e.Message);
         }
@@ -44,7 +89,7 @@ class App {
     }
 
     public void Run() {
-        //ModifyFile();
+        ModifyFile();
         Commit();
     }
 
@@ -58,6 +103,7 @@ class App {
             Console.WriteLine("请先初始化仓库");
             return;
         }
+        Commands.Stage(repository, TargetModifyFile);
         repository.Commit("modify", commit.Author, commit.Committer);
     }
 

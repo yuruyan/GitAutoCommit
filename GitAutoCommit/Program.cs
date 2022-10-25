6c70eb1723900782d8e53ae897bc42feeffaf638
Luxiu123 <klt5466@163.com>
2022-10-25 16:02:26 +0800

    refactor: 重构项目结构

--- GitAutoCommit/Program.cs
+++ GitAutoCommit/Program.cs
@@ -1,39 +1,30 @@
 ﻿using LibGit2Sharp;
 using Microsoft.Extensions.Configuration;
+using NLog;
 using System.Text;
 
 namespace GitAutoCommit;
 
-class App {
-    /// <summary>
-    /// 默认仓库路径
-    /// </summary>
-    private const string DefaultRepositoryPath = @"E:\Projects\CSharp\GitAutoCommit";
-    /// <summary>
-    /// 工作仓库路径
-    /// </summary>
-    private string RepositoryPath = DefaultRepositoryPath;
-    /// <summary>
-    /// 修改文件
-    /// </summary>
-    private string TargetModifyFile;
+public class App {
+    public readonly record struct CommandArg(
+        string RepoPath,
+        uint CommitTimes = DefaultCommitTimes,
+        // 相对于 RepoPath 路径
+        string ModifyFileName = DefaultModifyFileName
+    );
 
-    private record CommandArg {
-        public uint CommitTimes { get; set; } = 1;
-        public string RepoPath { get; set; } = string.Empty;
-    }
-
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+    private const int DefaultCommitTimes = 3;
+    private const string DefaultModifyFileName = "ForModify";
+    public readonly CommandArg ArgInfo;
     /// <summary>
-    /// 构造函数
+    /// 修改文件的绝对路径
     /// </summary>
-    /// <param name="repoPath">如果是 null 或者空串，则为 DefaultRepositoryPath</param>
-    /// <see cref="DefaultRepositoryPath"/>
-    public App(string repoPath = DefaultRepositoryPath) {
-        if (!string.IsNullOrWhiteSpace(repoPath)) {
-            RepositoryPath = repoPath;
-        }
-        TargetModifyFile = Path.Combine(RepositoryPath, @"GitAutoCommit\Demo.txt");
-        CheckRepository();
+    private readonly string ModifyFilePath;
+
+    public App(CommandArg argInfo) {
+        ArgInfo = argInfo;
+        ModifyFilePath = Path.Combine(argInfo.RepoPath, argInfo.ModifyFileName);
     }
 
     /// <summary>
@@ -42,18 +33,26 @@ class App {
     /// <param name="args"></param>
     /// <returns></returns>
     private static CommandArg ParseCommandArgs(string[] args) {
-        var configRoot = new ConfigurationBuilder().AddCommandLine(args).Build();
-        var repoPath = configRoot.GetSection("repo").Value;
-        var times = configRoot.GetSection("times").Value;
-        var result = new CommandArg() {
-            RepoPath = repoPath ?? string.Empty,
-        };
-        if (!string.IsNullOrWhiteSpace(times)) {
-            if (uint.TryParse(times, out var tempTimes)) {
-                result.CommitTimes = tempTimes;
-            }
+        var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+        var repoArg = config["repo"] ?? string.Empty;
+        var modifyFileName = config["modifyFile"] ?? DefaultModifyFileName;
+        var timesArg = config["times"] ?? DefaultCommitTimes.ToString();
+
+        if (!uint.TryParse(timesArg, out var commitTimes)) {
+            commitTimes = DefaultCommitTimes;
         }
-        return result;
+        // 验证
+        if (!Directory.Exists(repoArg)) {
+            Logger.Error($"仓库 '{repoArg}' 不存在！");
+            Environment.Exit(-1);
+        }
+        // 仓库是否合法
+        if (!Repository.IsValid(repoArg)) {
+            Logger.Error("给定路径不是合法仓库！");
+            Environment.Exit(-1);
+        }
+
+        return new(repoArg, commitTimes, modifyFileName);
     }
 
     /// <summary>
@@ -62,8 +61,13 @@ class App {
     /// </summary>
     /// <param name="args"></param>
     static void Main(string[] args) {
+        // 提示信息
+        if (args.Length == 0) {
+            Console.WriteLine(Resource.Resource.Help);
+            return;
+        }
         CommandArg commandArg = ParseCommandArgs(args);
-        var app = new App(commandArg.RepoPath);
+        var app = new App(commandArg);
         try {
             for (int i = 0; i < commandArg.CommitTimes; i++) {
                 app.Run();
@@ -72,16 +76,6 @@ class App {
             Console.WriteLine("success");
         } catch (Exception e) {
             Console.WriteLine(e.Message);
-        }
-    }
-
-    /// <summary>
-    /// 检查仓库是否合法
-    /// </summary>
-    /// <exception cref="Exception"></exception>
-    private void CheckRepository() {
-        if (!Repository.IsValid(RepositoryPath)) {
-            throw new Exception("给定路径不是合法仓库");
         }
     }
 
@@ -94,13 +88,13 @@ class App {
     /// 提交
     /// </summary>
     private void Commit() {
-        var repository = new Repository(RepositoryPath);
+        var repository = new Repository(ArgInfo.RepoPath);
         Commit? commit = repository.Commits.FirstOrDefault();
         if (commit is null) {
             Console.WriteLine("请先初始化仓库");
             return;
         }
-        Commands.Stage(repository, TargetModifyFile);
+        Commands.Stage(repository, ModifyFilePath);
         repository.Commit(
             "modify",
             new(commit.Author.Name, commit.Author.Email, DateTimeOffset.Now),
@@ -112,7 +106,7 @@ class App {
     /// 修改文件
     /// </summary>
     private void ModifyFile() {
-        using var stream = File.Open(TargetModifyFile, FileMode.Create, FileAccess.Write);
+        using var stream = File.Open(ModifyFilePath, FileMode.Create, FileAccess.Write);
         stream.Write(Encoding.ASCII.GetBytes($"{Random.Shared.NextDouble()}{DateTimeOffset.UtcNow.Millisecond}"));
     }
 }
